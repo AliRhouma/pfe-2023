0993707bafb1b686fee0a55d0ee7ad058f5674c9
Ali Rhouma <edurhoumaali@gmail.com>
2023-05-01 15:59:38 +0100

    childScreen

---
+++ app/src/main/java/com/example/pfe1/kidsView/data/remote/ChildRemote.kt
@@ -0,0 +1,8 @@
+package com.example.pfe1.kidsView.data.data_source.remote
+
+data class KidsRemote(
+    val id: String? = null,
+    val name: String? = null,
+    val school_year: String? = null,
+    val imageUrl : String? = null,
+)
